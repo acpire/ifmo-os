a9e90dbba95a85c53ce0ccd28179a74ea5b6a79b
eugene <extphy@gmail.com>
2015-11-30 10:18:36 +0300

    lab 4 fix for 32bit

--- lab4/cache.cpp
+++ lab4/cache.cpp
@@ -4,20 +4,20 @@
 
 #define N 100
 
-int sum_array_3d(int a[N][N][N])
+int sum_array_3d(int *a)
 { 
   int i, j, k, sum = 0; 
   for (i = 0; i < N; i++) 
     for (j = 0; j < N; j++) 
       for (k = 0; k < N; k++) 
-        sum += a[k][i][j];
+        sum += a[k + i*N*N + j*N];
 //        sum += a[i][j][k];
   return sum; 
 } 
 
 
 int main() {
-  int a[N][N][N];
+  int *a = (int*)malloc(sizeof(int) * N * N * N);
 
   double start = omp_get_wtime();
   for (int i=0; i<1000; ++i) {
